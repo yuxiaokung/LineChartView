f2f2ee28fbb260cd1c1a7ded6b3b8dd285a593e7
yukunlin <350308531@qq.com>
2018-08-03 11:23:38 +0800

    first commit

--- app/src/main/java/com/yukunlun/linechartview/MainActivity.java
+++ app/src/main/java/com/yukunlun/linechartview/MainActivity.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    private LineChartView mLineChartView;
     private static final String[] HORIZONTAL_AXIS= {"1", "2", "3", "4",
             "5", "6", "7", "8", "9", "10", "11", "12"};
 
@@ -14,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mLineChartView = findViewById(R.id.line_chart_view);
+        LineChartView mLineChartView = findViewById(R.id.line_chart_view);
         mLineChartView.setHorizontalAxis(HORIZONTAL_AXIS);
         mLineChartView.setDataList(DATA, 89);
     }
