ef175bfaba0fe654dfe1cb111232f836651213fe
yukunlin <350308531@qq.com>
2018-08-03 11:01:32 +0800

    first commit

---
+++ app/src/main/java/com/yukunlun/linechartview/Dot.java
@@ -0,0 +1,8 @@
+package com.yukunlun.linechartview;
+
+public class Dot {
+    int x;
+    int y;
+    int value;
+    int transformedValue;
+}
---
+++ app/src/main/java/com/yukunlun/linechartview/MainActivity.java
@@ -0,0 +1,21 @@
+package com.yukunlun.linechartview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private LineChartView mLineChartView;
+    private static final String[] HORIZONTAL_AXIS= {"1", "2", "3", "4",
+            "5", "6", "7", "8", "9", "10", "11", "12"};
+
+    private static final int[] DATA = {12, 24, 45, 56, 89, 70, 49, 22, 23, 10, 12, 3};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mLineChartView = findViewById(R.id.line_chart_view);
+        mLineChartView.setHorizontalAxis(HORIZONTAL_AXIS);
+        mLineChartView.setDataList(DATA, 89);
+    }
+}
